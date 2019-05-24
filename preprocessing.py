ce299b08e2e49db457455a8f803ad42ebfd83d63
daidai-gtlf <gootalife0512@gmail.com>
2019-05-24 19:22:16 +0900

    番号割り当て

--- preprocessing.py
+++ preprocessing.py
@@ -15,4 +15,19 @@ tokenizer.fit_on_texts(res)
 vocabulary = collections.OrderedDict(
     sorted(tokenizer.word_counts.items(), key=lambda x: x[1], reverse=True)
 )
-print(vocabulary)
+
+#単語と数値の対応表の作成
+table = collections.OrderedDict() #対応表
+num = 1 #各単語の添字
+threshold = 0 #閾値
+for key,val in vocabulary.items():
+    if val >= threshold:
+        table[key] = num
+    else:
+        table[key] = 0
+    num += 1
+for key,val in vocabulary.items():
+    print(key,val)
+print()
+for key,val in table.items():
+    print(key, val)
